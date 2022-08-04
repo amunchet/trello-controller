cba1b80952d74951600b9eb90cfb8bee33cb6c1d
Chester Enright <30327507+amunchet@users.noreply.github.com>
2022-08-04 13:25:02 -0500

    feat: added download attachments

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 import os
 import sys
 import dotenv
+import glob
+import shutil
+
+import requests
+
 from trello import TrelloClient as t
 
 dotenv.load_dotenv()
@@ -37,6 +42,51 @@ def main(listname, years=[2018,2019,2020]):
 
             print("Done with ", year, "for", listname)
 
+def download():
+    """Downloads all attachments"""
+    a = t(api_key=api_key, api_secret=api_secret, token=token)
+    
+    board_name = "Chester Priority"
+    list_name = "Fadal photos"
+    
+    # Get the board
+    priority = [x for x in a.list_boards() if x.name == board_name][0]
+    print("Priority: ", priority)
+    
+    # Get the lists
+    fadal_photos = [x for x in priority.list_lists() if x.name == list_name][0]
+    print("Lists:", fadal_photos)
+    
+    
+    # Get all cards that have a directory named here
+    files = [x.replace("\\", "") for x in glob.glob("*/") if "venv" not in x]
+    print("File:", files)
+    
+    cards = [x for x in fadal_photos.list_cards()]
+    print("Cards:", cards)
+    
+    headers = {
+        "Authorization" : "OAuth oauth_consumer_key=\"{}\", oauth_token=\"{}\"".format(api_key, token)
+    }
+    # Download all attachments to the directories
+    for file in files:
+        found = [x for x in cards if x.name == file]
+        if found:
+            print("Found a matching card:", file)
+            found = found[0]
+            
+            for (idx, attachment) in enumerate(found.get_attachments()):
+                with requests.get(attachment.url, headers=headers, stream=True) as r:
+                    with open(os.path.join(file, "{}.jpg".format(idx)), "wb") as f:
+                        shutil.copyfileobj(r.raw, f)
+                        print("Wrote out to {}.jpg".format(idx))
+                
+            
+    
 if __name__ == "__main__":
+    download()
+    """
     if len(sys.argv) > 1:
         main(sys.argv[1])
+    """
+    
